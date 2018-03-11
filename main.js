fe7ddac968ee739c359a3e754f483c2ecad2c945
Danielle Danskin <danielle.danskin@gmail.com>
2018-03-11 18:25:38 -0400

    update html and js insertion sort

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-//Bubble sort
+
+// Bubble sort
 function bubbleSort(arr) {
     let n = arr.length;
-    let swapped = true;
-    while (swapped) {
-        swapped = false;
+    let items_swapped;
+    do {
+        items_swapped = false;
         for (let i = 0; i < n-1; i++) {
             if (arr[i+1] < arr[i]) {
                 let temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
-                swapped = true;
+                items_swapped = true;
             }
+        }
+    } while (items_swapped);
+}
+
+// Insertion sort
+function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        let prev = arr[i];
+        while (arr[i+1] < prev) {
+            let temp = prev;
+            prev = arr[i+1];
+            arr[i+1] = temp;
         }
     }
 }
+
 
